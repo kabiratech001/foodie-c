a5aeb89ce4a3ea4928fe2dd5e713982097da51ff
kabiratech001 <161125959+kabiratech001@users.noreply.github.com>
2024-04-15 14:58:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,11 @@ const port = 4000
 
 // middlewares
 app.use(express.json())
-app.use(cors())
+app.use(
+  cors({
+    origin: "https://foodie-crus.onrender.com/",
+  })
+);
 
 // db connection
 connectDB()
